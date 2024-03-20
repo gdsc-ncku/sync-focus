352c23affb1a1bbce6afa64e3769fdb6cc0574f3
peterxcli <peterxcli@gmail.com>
2024-03-20 21:40:35 +0800

    fix: Integrating Jaeger with OpenTelemetry in Python Services
    - Update Jaeger service ports in `docker-compose.yml` to include UDP ports and additional TCP ports for tracing.
    - Enhance `otp.py` with comprehensive OpenTelemetry tracing setup, including Jaeger exporter configuration and tracing provider setup.
    - Replace basic tracer initialization with a detailed setup that includes Jaeger exporter, service name specification, and batch span processor in `otp.py`.
    - Modify the `trace` decorator in `otp.py` to use the updated OpenTelemetry API and improve error status handling.

--- otp/otp.py
+++ otp/otp.py
@@ -1,6 +1,24 @@
-import opentelemetry
+import opentelemetry.trace
+import opentelemetry.trace.status
+from opentelemetry.exporter.jaeger.thrift import JaegerExporter
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
-tracer = opentelemetry.trace.get_tracer("edu.ncku.gdsc.gcp.sync-focus.backend")
+jaeger_exporter = JaegerExporter(
+    agent_host_name="localhost",
+    agent_port=6831,
+)
+
+tracer_provider = TracerProvider(
+    resource=Resource.create({SERVICE_NAME: "sync-focus_backend"})
+)
+tracer_provider.add_span_processor(BatchSpanProcessor(jaeger_exporter))
+tracer = opentelemetry.trace.get_tracer(
+    instrumenting_module_name="edu.ncku.gdsc.gcp.sync-focus.backend",
+    tracer_provider=tracer_provider,
+)
+
 
 def trace(span_name, **attrs):
     def decorator(f):
@@ -8,10 +26,16 @@ def trace(span_name, **attrs):
             with tracer.start_as_current_span(span_name) as decorated_span:
                 for key, value in attrs.items():
                     decorated_span.set_attribute(key, str(value))
-                try:                      
+                try:
                     return f(*args, **kwargs)
                 except Exception as err:
-                    decorated_span.set_status(opentelemetry.trace.status.Status(opentelemetry.trace.status.StatusCode.ERROR, str(err)))
+                    decorated_span.set_status(
+                        opentelemetry.trace.status.Status(
+                            opentelemetry.trace.status.StatusCode.ERROR, str(err)
+                        )
+                    )
                     raise err
+
         return wrapper
+
     return decorator
