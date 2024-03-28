61daa312c4f00dc188cc1ee0920ac0c2de5d2b5b
peterxcli <peterxcli@gmail.com>
2024-03-28 21:38:32 +0800

    fix: Remove unique constraint on user_id in Heartbeat model

--- models/heartbeat.py
+++ models/heartbeat.py
@@ -1,17 +1,17 @@
 from datetime import datetime
 from uuid import uuid4
 
-from sqlalchemy import DateTime, ForeignKey, func
+from sqlalchemy import DateTime, func
 from sqlalchemy.orm import Mapped, mapped_column
 
 from .base import Base
-from .user import User
 
 
 class Heartbeat(Base):
     __tablename__ = "heartbeats"
     id: Mapped[str] = mapped_column(primary_key=True, index=True, default=uuid4().hex)
-    user_id: Mapped[str] = mapped_column(ForeignKey(User.id), unique=True)
+    # user_id: Mapped[str] = mapped_column(ForeignKey(User.id), unique=True)
+    user_id: Mapped[str] = mapped_column(index=True)
     entity: Mapped[str] = mapped_column(index=True)
     type: Mapped[str] = mapped_column(index=True)
     category: Mapped[str] = mapped_column(index=True)
