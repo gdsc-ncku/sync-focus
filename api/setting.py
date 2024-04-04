cb83594816622047001f286cb721f4d9c3020c26
peterxcli <peterxcli@gmail.com>
2024-04-05 01:45:11 +0800

    chore: Refactor API settings format

--- api/setting.py
+++ api/setting.py
@@ -1,8 +1,6 @@
-from typing import List
+from fastapi import APIRouter, Depends, Query
 
-from fastapi import APIRouter, Depends, HTTPException, Query, status
-
-from schemas.setting import UpdateSettingRequest,CreateSettingRequest,Setting
+from schemas.setting import CreateSettingRequest, Setting, UpdateSettingRequest
 from service.setting import SettingService
 
 # from auth.action import get_current_user
@@ -10,19 +8,28 @@ from .services import get_setting_service
 
 router = APIRouter(prefix="/settings", tags=["users"])
 
+
 @router.post("/update")
-def update_setting(new_setting: UpdateSettingRequest, 
+def update_setting(
+    new_setting: UpdateSettingRequest,
     setting_service: SettingService = Depends(get_setting_service),
-    user_id: str = Query(...),):
-    return setting_service.update_setting(user_id,new_setting.rev,new_setting.raw)
+    user_id: str = Query(...),
+):
+    return setting_service.update_setting(user_id, new_setting.rev, new_setting.raw)
+
 
 @router.post("/create")
-def create_setting(setting: CreateSettingRequest, 
+def create_setting(
+    setting: CreateSettingRequest,
     setting_service: SettingService = Depends(get_setting_service),
-    user_id: str = Query(...),):
-    return setting_service.create_setting(user_id,setting.raw)
+    user_id: str = Query(...),
+):
+    return setting_service.create_setting(user_id, setting.raw)
 
-@router.get("",response_model=Setting)
-def get_setting(setting_service: SettingService = Depends(get_setting_service),
-    user_id: str = Query(...),):
+
+@router.get("", response_model=Setting)
+def get_setting(
+    setting_service: SettingService = Depends(get_setting_service),
+    user_id: str = Query(...),
+):
     return setting_service.get_by_user(user_id)
