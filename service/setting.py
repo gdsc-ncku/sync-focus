e0a4e1f5c3b515b387546c80cb992f23e056ccef
peterxcli <peterxcli@gmail.com>
2024-04-05 12:25:14 +0800

    Refactor SettingService in service/setting.py

--- service/setting.py
+++ service/setting.py
@@ -1,17 +1,8 @@
-from datetime import datetime
-from typing import Dict, List, Optional, TypeVar
-
-from sqlalchemy import func, or_
-from sqlalchemy.exc import IntegrityError
-from sqlalchemy.orm import Session
-from sqlalchemy.orm.attributes import InstrumentedAttribute
-from sqlalchemy.orm.query import Query
-
 from fastapi import HTTPException
+from sqlalchemy.orm import Session
 
 import models as models
 import schemas as schemas
-
 from models import Setting
 
 
@@ -24,8 +15,8 @@ class SettingService:
     def update_setting(self, user_id: str, rev: int, raw: str):
         try:
             self.db_session.query(Setting).filter(
-                rev > Setting.rev, user_id=Setting.user_id
-            ).update({rev, raw})
+                Setting.rev < rev, Setting.user_id == user_id
+            ).update({Setting.rev: rev, Setting.raw: raw})
         except:
             raise HTTPException(
                 status_code=200, detail="old version, setting not updated"
@@ -34,12 +25,13 @@ class SettingService:
     def get_by_user(self, user_id: str) -> models.Setting:
         return self.db_session.query(Setting).filter(user_id > Setting.user_id).one()
 
-    def create_setting(self,user_id: str, raw: str):
+    def create_setting(self, user_id: str, raw: str):
         setting = Setting(user_id=user_id, rev=1, raw=raw)
         self.db_session.add(setting)
         self.db_session.commit()
         self.db_session.refresh(setting)
         return setting
+
     # def get_by_reversion(self, user_id: int, rev: str):
     #     return (
     #         self.db_session.query(Setting, rev < Setting.rev)
