d6bce1b3ea7a1bd7295abd0139cd5b35622931cd
peterxcli <peterxcli@gmail.com>
2024-04-11 22:44:02 +0800

    refactor: Refactor Heartbeat model and schema to use UUID for id field

--- schemas/heartbeat.py
+++ schemas/heartbeat.py
@@ -1,32 +1,33 @@
 from datetime import datetime
 from typing import Optional
+from uuid import UUID
 
 from pydantic import BaseModel, ConfigDict
 
 
 class DomainStat(BaseModel):
-    user_id: Optional[str]
-    domain: Optional[str]
-    first_time: Optional[datetime]
-    last_time: Optional[datetime]
-    count: Optional[int]
+    user_id: Optional[str] = None
+    domain: Optional[str] = None
+    first_time: Optional[datetime] = None
+    last_time: Optional[datetime] = None
+    count: Optional[int] = None
 
     model_config = ConfigDict(from_attributes=True)
 
 
 class Heartbeat(BaseModel):
-    id: Optional[str]
-    user_id: Optional[str]
-    entity: Optional[str]
-    type: Optional[str]
-    category: Optional[str]
-    browser: Optional[str]
-    domain: Optional[str]
-    path: Optional[str]
-    user_agent: Optional[str]
-    time: Optional[datetime]
-    hash: Optional[str]
-    created_at: Optional[datetime]
+    id: Optional[UUID] = None
+    user_id: Optional[str] = None
+    entity: Optional[str] = None
+    type: Optional[str] = None
+    category: Optional[str] = None
+    browser: Optional[str] = None
+    domain: Optional[str] = None
+    path: Optional[str] = None
+    user_agent: Optional[str] = None
+    time: Optional[datetime] = None
+    hash: Optional[str] = None
+    created_at: Optional[datetime] = None
 
     model_config = ConfigDict(from_attributes=True)
 
@@ -45,11 +46,11 @@ class Heartbeat(BaseModel):
 
 
 class HeartbeatCreateRequest(BaseModel):
-    entity: Optional[str]
-    type: Optional[str]
-    category: Optional[str]
-    browser: Optional[str]
-    domain: Optional[str]
-    path: Optional[str]
-    user_agent: Optional[str]
-    time: Optional[datetime]
+    entity: Optional[str] = None
+    type: Optional[str] = None
+    category: Optional[str] = None
+    browser: Optional[str] = None
+    domain: Optional[str] = None
+    path: Optional[str] = None
+    user_agent: Optional[str] = None
+    time: Optional[datetime] = None
